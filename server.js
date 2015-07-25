5bb84dd9bdcdade17ad57d4738b98a2779c8fddb
psheborskiy <psheborskiy@gmail.com>
2015-07-25 10:27:05 +0300

    port 80

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', {root: __dirname });
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address;
   var port = server.address().port;
